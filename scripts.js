f79f5b2904b94239032223bdc99af7b672fb5d95
Katharine Francis <katharinefrancis@Katharines-MBP.fios-router.home>
2017-08-10 09:06:52 -0400

    first push

---
+++ scripts.js
@@ -0,0 +1,19 @@
+// Question 1 : console.log every character in array 
+
+// var arr = ['k', 'a', 't', 'i', 'e', 'a', 'n', 'd', 's', 'a', 'u', 'r', 'a', 'v'];
+
+// for (i = 0; i < arr.length; i = i + 1) {
+// 	console.log(arr[i]);
+// }
+
+
+//Question 2: console.log 
+
+var string = "Katie and Saurav";
+
+for (i = 0; i < string.length; i = i + 1) {
+	if (string[i] !== " ") {
+		console.log(string[i]);
+	}
+	// var arr = string.split('').join('');
+}
